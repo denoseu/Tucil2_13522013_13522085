8ed1b5d854f6a7e9dbcceefe1e3ae4fac8db3f57
denoseu <denisefeliciatiowanni@gmail.com>
2024-03-13 21:19:50 +0700

    update: divide-n-conquer n titik

--- src/tes-n.py
+++ src/tes-n.py
@@ -4,10 +4,18 @@ from matplotlib.animation import FuncAnimation
 def midpoint(point1, point2):
     return ((point1[0] + point2[0]) / 2, (point1[1] + point2[1]) / 2)
 
+# menyimpan kurva sebelumnya supaya ga hilang (bisa di view)
+previous_curves = []
+
+# fungsi untuk menggambar bezier secara rekursif
 def draw_bezier_recursive(points, iterations):
     if len(points) == 2 or iterations == 0:
-        xs, ys = zip(*points) # unpack points jadi x and y coordinates
-        plt.plot(xs, ys, 'b-')
+        xs, ys = zip(*points)  # unpack points menjadi x and y coordinates
+        if iterations == 0:
+            plt.plot(xs, ys, 'b-')  # Garis normal untuk iterasi terakhir
+            # simpan kurva ini untuk iterasi berikutnya
+            previous_curves.append((xs, ys))
+        # tidak perlu lagi menyimpan kurva di sini karena kita tidak menggunakan previous_curves dalam fungsi ini
     else:
         new_points = []
         for i in range(len(points)-1):
@@ -15,10 +23,6 @@ def draw_bezier_recursive(points, iterations):
         new_points = [points[0]] + new_points + [points[-1]]
         draw_bezier_recursive(new_points, iterations - 1)
 
-# cntoh titik
-points = [(2, 2), (3, 3), (6, 2)]
-
-# buat kurva animasi
 def animate(iteration):
     ax.clear()
     ax.set_xlabel('X')
@@ -27,13 +31,20 @@ def animate(iteration):
     ax.grid()
     ax.axis('equal')
 
-    # Tampilkan titik
+    # tampilkan titik
     for i, point in enumerate(points):
         plt.plot(*point, 'ro')
         plt.text(point[0], point[1], f'P{i}')
 
-    # Gambar kurva
+    # gambar ulang semua kurva sebelumnya dengan garis putus-putus (biar tau yang lalu lalu)
+    for curve in previous_curves[:iteration]:
+        plt.plot(curve[0], curve[1], '--', color='grey')
+
+    # gambar kurva saat ini
     draw_bezier_recursive(points, iteration)
+
+# contoh titik
+points = [(2, 2), (4, 4), (3, 3), (6, 2)]
 
 # inisialisasi plot
 fig, ax = plt.subplots()
@@ -45,7 +56,7 @@ ax.axis('equal')
 
 # buat animasi
 iterations = int(input("Masukkan jumlah iterasi: "))
-ani = FuncAnimation(fig, animate, frames=iterations+1, interval=350, repeat=True)
+ani = FuncAnimation(fig, animate, frames=iterations+1, interval=350, repeat=False)
 
 # tampilkan animasi
 plt.show()
