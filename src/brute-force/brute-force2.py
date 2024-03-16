dafce16d6a2c63eac54ab7590877a3d2270071e7
hiirrs <13522085@mahasiswa.itb.ac.id>
2024-03-16 09:47:52 +0700

    animate brute-force2.py

--- src/brute-force/brute-force2.py
+++ src/brute-force/brute-force2.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+from matplotlib.animation import FuncAnimation
 
 def binomial_coefficient(n, k):
     if 0 <= k <= n:
@@ -17,19 +18,22 @@ def bezier_curve_brute_force(t, points):
         result += binomial_coefficient(n, i) * ((1 - t) ** (n - i)) * (t ** i) * np.array(p)
     return result
 
-def plot_bezier_curve(points):
-    t_values = np.linspace(0, 1, 100)
-    curve_points = np.array([bezier_curve_brute_force(t, points) for t in t_values])
-    
-    plt.plot(curve_points[:,0], curve_points[:,1], label='Bézier Curve')
-    plt.plot([point[0] for point in points], [point[1] for point in points], 'ro-', label='Control Points')
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.title('Bézier Curve')
-    plt.legend()
-    plt.grid(True)
-    plt.axis('equal')
-    plt.show()
+def plot_bezier_curve(frame, ax, points):
+    ax.clear()
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_title(f'Beziér Curve Iteration {frame}')
+    ax.grid()
+    ax.axis('equal')
+    ax.plot([point[0] for point in points], [point[1] for point in points], 'ro-', label='Control Points')
+    for i in range(1, frame + 1):
+        t_values = np.linspace(0, 1, i*2+1)
+        curve_points = np.array([bezier_curve_brute_force(t, points) for t in t_values])
+        if i == frame:
+            ax.plot(curve_points[:,0], curve_points[:,1], color='red', label='Current Iteration')
+        else:
+            ax.plot(curve_points[:,0], curve_points[:,1], linestyle='dotted', color='gray')
+    ax.legend()
 
 def main():
     # Input n titik
@@ -50,8 +54,12 @@ def main():
 
     x_end, y_end = map(float, input("Masukan end point (x,y): ").split(","))
     points.append((x_end, y_end))
-    
-    plot_bezier_curve(points)
+
+    iteration = int(input("Masukan jumlah iterasi: "))
+
+    fig, ax = plt.subplots()
+    ani = FuncAnimation(fig, plot_bezier_curve, frames=iteration+1, fargs=(ax, points), interval=500, repeat=False)
+    plt.show()
 
 if __name__ == "__main__":
     main()
