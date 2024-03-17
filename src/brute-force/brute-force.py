58863158e7f44227c383a418ac2e225287695f70
denoseu <denisefeliciatiowanni@gmail.com>
2024-03-17 12:53:17 +0700

    Update brute-force.py

--- src/brute-force/brute-force.py
+++ src/brute-force/brute-force.py
@@ -41,39 +41,34 @@ def update(frame, ax, points, curves, iterations, execution_time):
     execution_time_info = f'Execution Time: {execution_time:.2f} seconds'
     ax.text(0.95, 0.05, execution_time_info, transform=ax.transAxes, ha='right', va='bottom')
 
-
-def main():
-    # Input n titik
+# Input n titik
+n = int(input("Masukkan jumlah titik yang hendak dimasukkan: "))
+while n < 2:
+    print("\nUntuk membuat kurva masukkan 2 atau lebih titik!")
     n = int(input("Masukkan jumlah titik yang hendak dimasukkan: "))
-    while n < 2:
-        print("\nUntuk membuat kurva masukkan 2 atau lebih titik!")
-        n = int(input("Masukkan jumlah titik yang hendak dimasukkan: "))
 
-    # Input koordinat poin-poin
-    x_start, y_start = map(float, input("Masukkan start point (x,y): ").split(","))
-    points = [(x_start, y_start)]
+# Input koordinat poin-poin
+x_start, y_start = map(float, input("Masukkan start point (x,y): ").split(","))
+points = [(x_start, y_start)]
 
-    # control points
-    for i in range(n-2):
-        x, y = map(float, input("Masukkan control point {}: (x,y): ".format(i+1)).split(","))
-        points.append((x, y))
-    
-    # end points
-    x_end, y_end = map(float, input("Masukkan end point (x,y): ").split(","))
-    points.append((x_end, y_end))
+# control points
+for i in range(n-2):
+    x, y = map(float, input("Masukkan control point {}: (x,y): ".format(i+1)).split(","))
+    points.append((x, y))
 
-    # banyak iterasi
-    iterations = int(input("Masukkan jumlah iterasi: "))
+# end points
+x_end, y_end = map(float, input("Masukkan end point (x,y): ").split(","))
+points.append((x_end, y_end))
 
-    start_time = timeit.default_timer()
-    curves = bezier_curve_points(points, iterations+1)
-    
-    fig, ax = plt.subplots()
+# banyak iterasi
+iterations = int(input("Masukkan jumlah iterasi: "))
 
-    execution_time = timeit.default_timer() - start_time
-    
-    ani = FuncAnimation(fig, update, frames=iterations+1, fargs=(ax, points, curves, iterations, execution_time), interval=150, repeat=False)
-    plt.show()
+start_time = timeit.default_timer()
+curves = bezier_curve_points(points, iterations+1)
 
-if __name__ == "__main__":
-    main()
+fig, ax = plt.subplots()
+
+execution_time = timeit.default_timer() - start_time
+
+ani = FuncAnimation(fig, update, frames=iterations+1, fargs=(ax, points, curves, iterations, execution_time), interval=150, repeat=False)
+plt.show()
